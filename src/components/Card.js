6e7cfbe448e8e4f65f490e1c06645301964a8e74
misztijanos <misztijanos@yahoo.com>
2021-08-08 10:35:44 +0300

    created the front page and a card component

---
+++ src/components/Card.js
@@ -0,0 +1,21 @@
+import styles from './Card.module.css'
+const Card = () => {
+  return (
+    <div className={styles.card}>
+      <h3 className={styles.card_title}>City Name</h3>
+      <div className={styles.card_body}>
+        <div className={styles.card_main}>
+          <i></i>
+          <p>12~</p>
+        </div>
+        <p>Partly Cloudy, Gentle Breeze</p>
+        <div className={styles.card_data}>
+          <p>Wind speed</p>
+          <p>Wind direction</p>
+          <p>Cloudcover</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Card
